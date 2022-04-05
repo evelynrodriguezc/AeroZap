5f680e4a28b2190400fb2e5658adc413a13fc77c
Evelyn R <evelynrodriguezc0@gmail.com>
2022-04-05 17:51:04 -0500

    recommendations section

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  content: ['./public/*.{html,js}'],
+  theme: {
+    textColor: {
+      primary: "#CC2D4A",
+      secondary: "#8FA206",
+      terciary: "#61AEC9",
+      white: "#FFFF"
+    },
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      }),
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  ringColor:{
+    'primary': '#CC2D4A',
+    'secondary': '#8FA206',
+    'tertiary': '#61AEC9',
+  },  
+  plugins: [],
+};
